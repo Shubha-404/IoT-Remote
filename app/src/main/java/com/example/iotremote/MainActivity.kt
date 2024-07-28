d9a46e24243979a386de6321424242489211f8eb
Shubha-404 <140900333+Shubha-404@users.noreply.github.com>
2024-07-28 22:18:48 +0530

    clear all except AndroidManifest

--- app/src/main/java/com/example/iotremote/MainActivity.kt
+++ app/src/main/java/com/example/iotremote/MainActivity.kt
@@ -6,48 +6,18 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.annotation.RequiresApi
-import androidx.compose.runtime.Composable
-import com.example.iotremote.Bluetooth.BluetoothHelper
-import com.example.iotremote.Screens.BluetoothOnOff
-import com.example.iotremote.Screens.PairedDevices
 import com.example.iotremote.ui.theme.IoTRemoteTheme
 
 class MainActivity : ComponentActivity() {
-    lateinit var bluetoothHelper: BluetoothHelper
-
     @RequiresApi(Build.VERSION_CODES.S)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        bluetoothHelper = BluetoothHelper(this, activityResultRegistry)
-
         enableEdgeToEdge()
         setContent {
             IoTRemoteTheme {
-                App(bluetoothHelper)
+
             }
         }
     }
-} .
-
-@RequiresApi(Build.VERSION_CODES.S)
-@Composable
-fun App(bluetoothHelper: BluetoothHelper) {
-    BluetoothOnOff(
-        onTurnOnBluetooth = { bluetoothHelper.turnOnBluetooth() },
-        onTurnOffBluetooth = { bluetoothHelper.turnOffBluetooth() }
-    )
-    PairedDevices(bluetoothHelper = bluetoothHelper)
 }
 
-
-//@Preview(showBackground = true, showSystemUi = true)
-//@Composable
-//fun PreviewBluetoothOnOff() {
-//    IoTRemoteTheme {
-//        BluetoothOnOff(
-//            onTurnOnBluetooth = {},
-//            onTurnOffBluetooth = {}
-//        )
-//    }
-//}
